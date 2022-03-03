a6a36732814db8c6b3231a6b78e8e9b42b2c1fb4
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-02 22:25:09 -0500

    moving completed social media dashboard to deoplyment branch

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-// => Elements
-const toggleBtn = document.querySelector('.header__content--btn');
-const priceCards = document.querySelectorAll('.pricing__card--header__active');
+console.log('Hello');
 
-/**
- * adjustPrice is an helper that toggles the cost each card monthly or annually.
- *
- * @param {*} price
- * @param {*} index => the index is the number of 9 to add or remove from a price
- * @param {*} monthly : boolean
- * @returns String
- */
-const adjustPrice = (price, index, monthly) => {
-  const prices = price.split('.');
-  let newPrice;
-  if (monthly) {
-    newPrice =
-      prices[0].substring(0, prices[0].length - index) + '.' + prices[1];
-  } else {
-    newPrice = prices[0] + '9'.repeat(index) + '.' + prices[1];
-  }
-  return newPrice;
-};
+// => Elements
+const toggleBtn = document.getElementById('toggle--btn');
+const toggleText = document.querySelector('.toggle--status');
+const htmlElement = document.documentElement;
 
 if (toggleBtn) {
-  toggleBtn.addEventListener('click', () => {
-    // => Toggle the price for all price cards
-    priceCards.forEach((card) => {
-      // => Get the current price
-      const currentPrice = card.textContent.substring(
-        1,
-        card.textContent.length
-      );
-      // => Check the if it is a monthly plan
-      if (currentPrice < 50) {
-        card.textContent = `$${adjustPrice(currentPrice, 1, false)}`;
-      } else {
-        card.textContent = `$${adjustPrice(currentPrice, 1, true)}`;
-      }
-    });
+  toggleBtn.addEventListener('change', () => {
+    if (toggleBtn.checked) {
+      htmlElement.setAttribute('data-theme', 'dark');
+      toggleText.textContent = 'Light Mode';
+    } else {
+      htmlElement.setAttribute('data-theme', 'light');
+      toggleText.textContent = 'Dark Mode';
+    }
+
+    // => Apply smooth transition
+    smoothTransition();
   });
 }
+
+const smoothTransition = () => {
+  htmlElement.classList.add('transition');
+
+  window.setTimeout(() => {
+    htmlElement.classList.remove('transition');
+  }, 100);
+};
