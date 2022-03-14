f277129693af03de7bdf34b36f6a86f8798bf38b
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-14 18:30:53 -0400

    moving completed flex panel to deployment branch

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-'use strict';
-const image = document.querySelector('.main--image');
-const headerSpan = document.querySelector('h1 span');
-const inputFields = document.querySelectorAll('input');
+const panels = document.querySelectorAll('.panel');
 
-// Update Image
-const updateImage = (field) => {
-  const name = field.target.name;
-  const value = field.target.value;
-  if (name === 'range--spacing') {
-    const padding = `${value}px`;
-    document.documentElement.style.setProperty(`--${name}`, padding);
-  }
-
-  if (name === 'range--blur') {
-    const opacity = `${value}px`;
-    document.documentElement.style.setProperty(`--${name}`, opacity);
-  }
-
-  if (name === 'base--color') {
-    document.documentElement.style.setProperty(`--${name}`, value);
-  }
-};
-
-// => Add event listener to the input field checking for changes
-inputFields.forEach((field) => {
-  field.addEventListener('change', updateImage);
-  field.addEventListener('mousemove', updateImage);
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    panel.classList.toggle('open');
+  });
 });
+
+panels.forEach((panel) =>
+  panel.addEventListener('transitionend', (event) => {
+    const { propertyName } = event;
+    if (propertyName.includes('flex')) {
+      panel.classList.toggle('open-active');
+    }
+  })
+);
