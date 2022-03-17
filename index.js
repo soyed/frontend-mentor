68de224cb2a76a4c4098d7d349921631db9fa80b
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-16 21:01:24 -0400

    moving ajax head component to depolyment branch

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-// Elements
-const menuButton = document.querySelector('.hero__header--hamburger');
-const menuImage = document.getElementById('header__hamburger--icon');
-const menu = document.querySelector('.hero__header--nav');
-const heroHeader = document.querySelector('.hero__header');
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-// => toggle hamburger button
-if (menuButton) {
-  menuButton.addEventListener('click', () => {
-    heroHeader.classList.toggle('open');
-    menu.classList.toggle('open');
-    menuButton.classList.toggle('open');
+const cities = [];
 
-    // => Check grab the existing src
-    const iconStatus = menuImage.src.includes('icon-hamburger');
-    // => toggle the image src based on its src
-    if (iconStatus) {
-      menuImage.src = './images/icon-close.svg';
-    } else {
-      menuImage.src = './images/icon-hamburger.svg';
-    }
+const fetchData = async () => {
+  try {
+    const data = await fetch(endpoint);
+    const citiesJSON = await data.json();
+    cities.push(...citiesJSON);
+  } catch (e) {
+    console.log(e);
+  }
+};
+fetchData();
+
+const searchField = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+const findMatches = (matchWord) => {
+  return cities.filter((region) => {
+    const regex = new RegExp(matchWord, 'gi');
+    return region.city.match(regex) || region.state.match(regex);
   });
-}
+};
+
+const parsePopulation = (population) => {
+  return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+const displayCityState = (event) => {
+  const value = event.target.value;
+  const foundMatches = findMatches(value);
+
+  const html = foundMatches
+    .map((match) => {
+      const regex = new RegExp(value, 'gi');
+      const city = match.city.replace(
+        regex,
+        `<span class="hl">${value}</span>`
+      );
+      const state = match.state.replace(
+        regex,
+        `<span class="hl">${value}</span>`
+      );
+
+      return `
+        <li>
+            <span class="name">${city}, ${state}</span>
+            <span class="population">${parsePopulation(match.population)}</span>
+        </li>
+    `;
+    })
+    .join('');
+
+  suggestions.innerHTML = html;
+};
+
+searchField.addEventListener('change', displayCityState);
+searchField.addEventListener('keyup', displayCityState);
