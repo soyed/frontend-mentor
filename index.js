c7b4891eb04bda93a907e6bd865d9c6635875258
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-24 21:53:08 -0400

    completed responsive price functionality

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
-const checkBoxes = document.querySelectorAll("input[type='checkbox']");
+const toggleBtn = document.querySelector('.toggle--btn');
+const priceText = document.querySelector('.price');
+const priceSlider = document.querySelector('.slider-container');
+const priceFill = document.querySelector('.price__fill');
+const priceTrack = document.querySelector('.price__track');
 
-let lastChecked;
-
-const toggleCheckBox = (event) => {
-  const { shiftKey } = event;
-  const { checked } = event.target;
-
-  let inBetweenChecked = false;
-  if (shiftKey && checked) {
-    checkBoxes.forEach((checkBox) => {
-      if (checkBox === event.target || checkBox === lastChecked) {
-        inBetweenChecked = !inBetweenChecked;
-      }
-      if (inBetweenChecked) checkBox.checked = true;
-    });
-  } else {
-    checkBoxes.forEach((checkBox) => {
-      if (checkBox !== event.target) {
-        checkBox.checked = false;
-      }
-    });
-  }
-
-  lastChecked = event.target;
+/*==========  Helper Methods  ======*/
+let clicked = false,
+  toggled = false;
+const parseDefaultPrice = () => {
+  return priceText.textContent.slice(1).split('.')[0];
 };
 
-checkBoxes.forEach((checkBox) =>
-  checkBox.addEventListener('click', toggleCheckBox)
-);
+const parsePrice = (price) => {
+  if (priceText.dataset.month === 'month')
+    return (price + (price * 0.25) / 0.75) / 12;
+  else if (priceText.dataset.month === 'year') return price * 12 * 0.75;
+};
+
+const togglePrice = (price = parseDefaultPrice()) => {
+  price = Number(price);
+  priceText.textContent = `$${parsePrice(price)}.00`;
+};
+const updatePrice = (price) => {
+  if (priceText.dataset.month === 'year') {
+    priceText.textContent = `$${parsePrice(price)}.00`;
+  } else {
+    priceText.textContent = `$${price}.00`;
+  }
+};
+
+const slidePrice = (event) => {
+  const { offsetX } = event;
+  const width = Math.round((offsetX / priceTrack.clientWidth) * 100);
+  updatePrice(width);
+  priceFill.style.width = `calc(${width}% - 1.75rem)`;
+};
+
+const handleTogglePrice = () => {
+  toggleBtn.classList.toggle('toggle');
+  if (priceText.dataset.month === 'month') {
+    priceText.setAttribute('data-month', 'year');
+  } else {
+    priceText.setAttribute('data-month', 'month');
+  }
+  togglePrice();
+};
+
+/*==========  Event Listeners  ======*/
+
+// => List for price toggle => monthly / yearly
+toggleBtn.addEventListener('click', handleTogglePrice);
+
+// => track mousemove when the price slider is clicked
+let mousedown = false;
+
+priceSlider.addEventListener('mouseup', () => (mousedown = false));
+priceSlider.addEventListener('mousedown', () => (mousedown = true));
+
+priceTrack.addEventListener('mousemove', (event) => {});
+priceTrack.addEventListener('click', slidePrice);
